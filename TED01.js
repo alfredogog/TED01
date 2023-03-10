e6c5616cd208cab51aa7f4e65f82e441bb74adfc
alfredogog <alfredgog93@gmail.com>
2023-03-10 16:04:18 -0300

    TED

--- TED01.js
+++ TED01.js
@@ -2,7 +2,7 @@ var i, altura, sexo, mulher = 0, quantidadeMulher = 0, homem = 0, quantidadeHome
 var menorAltura = 0, maiorAltura = 0, mediaAlturaHomem;
 
 
-for(i = 1; i <= 3; i++){
+for(i = 1; i <= 15; i++){
     altura=parseFloat(prompt("Informe a sua altura: " + i));
     sexo=prompt("Informe o seu sexo (M ou F): ").toUpperCase();
 
