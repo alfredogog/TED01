a818acd2b882e8bc370f956d93168eb42081b312
alfredogog <alfredgog93@gmail.com>
2023-03-10 15:53:07 -0300

    Quarto commit

--- TED01.js
+++ TED01.js
@@ -1,26 +1,29 @@
-var i, altura, sexo, maiorAltura = 0, menorAltura = 0, mulher = 0, quantidadeMulher = 0, homem = 0, quantidadeHomem = 0, mediaAlturaHomem;
+var i, altura, sexo, mulher = 0, quantidadeMulher = 0, homem = 0, quantidadeHomem = 0;
+var menorAltura = 0, maiorAltura = 0, mediaAlturaHomem;
 
-for(i=1; i<=15; i++){
-    altura=parseFloat(prompt("Informe a sua altura: ["+i+"]"));
-    sexo=prompt("Informe o seu sexo (M ou F): ");
 
-    if(i==1){
-        menorAltura=altura;
-    } if(altura>maiorAltura){
-        maiorAltura=altura;
-    } if(altura<menorAltura){
-        menorAltura=altura;
-    } if(sexo=='F'){
-        mulher+=altura;
+for(i = 1; i <= 15; i++){
+    altura=parseFloat(prompt("Informe a sua altura: " + i));
+    sexo=prompt("Informe o seu sexo (M ou F): ").toUpperCase();
+
+    if(i == 1){
+        menorAltura = altura;
+    } if(altura > maiorAltura){
+        maiorAltura = altura;
+    } if(altura < menorAltura){
+        menorAltura = altura;
+    } if(sexo === 'F'){
+        mulher += altura;
         quantidadeMulher++;
-    } else if(sexo=='M'){
-        homem+=altura;
+    } else if(sexo === 'M'){
+        homem += altura;
         quantidadeHomem++;
     }
 }
+
 mediaAlturaHomem=homem/quantidadeHomem;
 
-console.log("A maior ++ altura do grupo é: "+maiorAltura);
-console.log("A menor altura do grupo é: "+menorAltura);
-console.log("A média da altura dos homens é: "+mediaAlturaHomem);
-console.log("O número de mulheres é: "+quantidadeMulher);
+console.log("A maior altura do grupo é: " + maiorAltura);
+console.log("A menor altura do grupo é: " + menorAltura);
+console.log("A média da altura dos homens é: " + mediaAlturaHomem);
+console.log("O número de mulheres é: " + quantidadeMulher);
